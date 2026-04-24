301c72b8d75592f4e19cbe5f25981b7072fc32df
EngelberAmaya <engelberamaya@gmail.com>
2026-04-24 15:22:04 -0400

    UpperCase, LowerCase and TitleCase Pipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.html',
   styleUrl: './basic-page.css',
 })
 export default class BasicPage {
-
+  nameLower = signal('engelber');
+  nameUpper = signal('ENGELBER');
+  fullName = signal('EngeLbeR AmaYa');
 }
