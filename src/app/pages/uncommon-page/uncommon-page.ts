a7a4e2e4fb018920423035deb3ce56846d1a37a5
EngelberAmaya <engelberamaya@gmail.com>
2026-04-27 12:57:20 -0400

    I18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Engelber',
+  gender: 'male',
+  age: 35,
+  address: 'Barquisimeto, Venezuela'
+}
+
+const client2 = {
+  name: 'Maria',
+  gender: 'female',
+  age: 27,
+  address: 'Caracas, Venezuela'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card, I18nSelectPipe ],
   templateUrl: './uncommon-page.html',
   styleUrl: './uncommon-page.css',
 })
 export default class UncommonPage {
+  // i18n Select
+  client = signal(client1);
 
+  invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla',
+  }
+
+  changeClient() {
+
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
 }
