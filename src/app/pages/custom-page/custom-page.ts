fcaeca2014216ba38abef815ae52d77f4aacb2a6
EngelberAmaya <engelberamaya@gmail.com>
2026-04-28 12:56:55 -0400

    Custom Pipe - HeroTextColor

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -3,10 +3,21 @@ import { ToggleCasePipe } from '../../pipes/toggle-case-pipe';
 import { heroes } from '../../data/heroes-data';
 import { CanFlyPipe } from '../../pipes/can-fly-pipe';
 import { HeroColorPipe } from '../../pipes/hero-color-pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color-pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator-pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe ],
+  imports: [
+    ToggleCasePipe,
+    CanFlyPipe,
+    HeroColorPipe,
+    HeroTextColorPipe,
+    TitleCasePipe,
+    HeroCreatorPipe,
+    HeroColorPipe
+  ],
   templateUrl: './custom-page.html',
   styleUrl: './custom-page.css',
 })
