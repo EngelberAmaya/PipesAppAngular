2e0e4b1a1be092e753bfcf156f7657b4109572c1
EngelberAmaya <engelberamaya@gmail.com>
2026-04-28 11:29:25 -0400

    Preparing the data

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case-pipe';
+import { heroes } from '../../data/heroes-data';
 
 @Component({
   selector: 'app-custom-page',
@@ -10,4 +11,5 @@ import { ToggleCasePipe } from '../../pipes/toggle-case-pipe';
 export default class CustomPage {
   name = signal('Engelber Amaya');
   upperCase = signal(true);
+  heroes = signal(heroes);
 }
